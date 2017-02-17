3d2ea3b4a1ca16eff36f185bf5a1bacaa3a5283d
Maclean Pinto <macleanmauricepinto@gmail.com>
2017-02-17 12:35:04 +0530

    Adding Employee Name to Seat

--- src/app/block/block.component.ts
+++ src/app/block/block.component.ts
@@ -12,51 +12,61 @@ export class BlockComponent implements OnInit {
             {
                 no: '1A',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1B',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: false
             },
             {
                 no: '1C',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1D',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1E',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1F',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1G',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1H',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1I',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '1J',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
         ]
@@ -67,6 +77,7 @@ export class BlockComponent implements OnInit {
             {
                 no: '2A',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
@@ -75,46 +86,55 @@ export class BlockComponent implements OnInit {
             {
                 no: '2B',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '2C',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '2D',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '2E',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '2F',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '2G',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: true
             },
             {
                 no: '2H',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: false
             },
             {
                 no: '2I',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: false
             },
             {
                 no: '2J',
                 pname: 'RI',
+                ename: 'Maclean Pinto',
                 disabled: false
             },
         ]
